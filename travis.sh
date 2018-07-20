d92c19c9246502bb3b0cd763e605fd3744114899
曹云扬 <caoyy@easemob.com>
2018-07-20 12:32:55 +0800

    m

--- travis.sh
+++ travis.sh
@@ -3,5 +3,5 @@ echo gh_token: ${gh_token}
 
 echo 'start deploy'
 git remote rm origin
-git remote add origin https://index-swf:${gh_token}@github.com/index-swf/test-github-pages.git
+git remote add origin https://index-swf:58b8b008491a8f0e5d59b4b64c0c83c9786c480f@github.com/index-swf/test-github-pages.git
 git push origin master:refs/heads/gh-pages
